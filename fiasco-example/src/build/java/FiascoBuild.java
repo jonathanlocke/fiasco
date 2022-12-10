05dab1e087c9549e0eea8926758d2747f6e735a1
jonathanl-telenav <jonathanl@telenav.com>
2022-12-10 10:39:27 -0700

    work in progress on mockup

--- fiasco-example/src/build/java/FiascoBuild.java
+++ fiasco-example/src/build/java/FiascoBuild.java
@@ -3,17 +3,16 @@ import digital.fiasco.runtime.build.metadata.Contributor;
 import digital.fiasco.runtime.build.metadata.Copyright;
 import digital.fiasco.runtime.build.metadata.Metadata;
 import digital.fiasco.runtime.build.metadata.Organization;
-import digital.fiasco.runtime.build.BuildRepositories;
+
+import static digital.fiasco.runtime.build.tools.librarian.Library.library;
 
 /**
  * Example Fiasco build.
  *
  * @author jonathan
  */
-@SuppressWarnings({ "InnerClassMayBeStatic", "unused" })
-public class FiascoBuild extends Build implements
-        Libraries,
-        BuildRepositories
+@SuppressWarnings({ "InnerClassMayBeStatic", "unused", "SpellCheckingInspection" })
+public class FiascoBuild extends Build
 {
     public static void main(String[] arguments)
     {
@@ -24,13 +23,14 @@ public class FiascoBuild extends Build implements
     public void onBuildStart()
     {
         metadata(new Metadata("fiasco:fiasco-example:1.0")
-                .withOrganization(new Organization("State of the Art")
-                        .withUrl("https://www.state-of-the-art.org"))
+                .withOrganization(new Organization("Digital Fiasco")
+                        .withUrl("https://www.fiasco.digital"))
                 .withCopyright(new Copyright("Copyright 2020-2022, Jonathan Locke. All Rights Reserved."))
                 .withContributor(new Contributor("Jonathan Locke")
-                        .withRole("BuildProject Lead")
+                        .withRole("Project Lead")
                         .withEmail("jon@thanlocke.com")));
 
-        librarian().lookIn(mavenCentral);
+        libraries().add(library("org.apache.commons:logging:1.0"));
+        libraries().add(library("com.esotericsoftware:kryo:4.3.1"));
     }
 }
